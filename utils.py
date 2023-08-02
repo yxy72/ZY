b8f388c0274f67e2f99c5748ec715b0c80d878aa
yxy39 <1115886391@qq.com>
2023-08-02 11:51:07 +0800

    2

--- utils.py
+++ utils.py
@@ -7,6 +7,40 @@ import os
 import pickle
 
 
+
+# 该类用于即时中止模型过拟合
+class EarlyStopping:
+    
+    def __init__(self,filename,save_path="./项目文件/model/", patience=100, delta=0):
+        self.save_path = save_path
+        self.patience = patience
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.delta = delta
+        self.filename = filename
+
+    def __call__(self, val_acc, model):
+        
+        score = val_acc
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint( model)
+            self.counter = 0
+
+    def save_checkpoint(self, model):
+
+        path = os.path.join(self.save_path, self.filename)
+        torch.save( model.state_dict(), path)	# 这里会存储迄今最优模型的参数
+
+
 def maxPooling(arr,targetSize):
     size = len(arr)/targetSize
     if(len(arr)%targetSize!=0):
@@ -164,3 +198,87 @@ class MODEL_LSTM_EDA(nn.Module):
         x = x[:, -1, :]
         x = self.Linear(x)
         return x
+    
+
+    
+def TRAIN(model,modelName,target,KFSORT,epochs=500,lr=0.0001,scheduler_step_size=100,scheduler_gamma=0.97):
+    
+    #model = MODEL_CNN_EDA(input_channel=1)
+    model = model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr) #0.0008
+
+    criterion = torch.nn.CrossEntropyLoss()
+
+    scheduler = StepLR(optimizer, step_size=scheduler_step_size, gamma=scheduler_gamma)
+
+    lossHistory = []
+    accHistory = []
+    trainAccHis = []
+
+
+
+    early_stopping = EarlyStopping(patience=200,filename = modelName+"_"+target+"_5("+str(KFSORT+1)+").pth")
+
+    for epoch in range(0,epochs):
+        model.train()
+        LOSS = 0
+        ACC = 0
+        for batch_data, batch_targets in dataLoader_train:
+            optimizer.zero_grad()
+            outputs = model(batch_data.to(device))
+            loss = criterion(outputs, batch_targets.to(device))
+            loss.backward()
+
+            LOSS += loss.detach().cpu().numpy()
+            ACC += (torch.max(outputs,dim=1)[1] == batch_targets.to(device)).sum()
+
+            optimizer.step()
+
+        scheduler.step()
+
+        lossHistory.append(LOSS)
+
+        print(f"epoch{epoch+1}: {str(LOSS)[0:7]}",end="\t ")
+        cal = (ACC/dataCount).cpu()
+        print(f"acc_t[{str(np.array(cal))[0:5]}]",end="\t ")
+        trainAccHis.append(cal)
+        print("lr["+str(optimizer.param_groups[0]['lr'])+"]",end="\t ")
+
+    #     for p in model.parameters():
+    #         print(p.grad.data)
+
+        model.eval()
+        with torch.no_grad():
+            all_outputs = []
+            all_targets = []
+            for batch_data, batch_targets in dataLoader_valid:
+                outputs = model(batch_data.to(device))
+                _, predicted = torch.max(outputs.data, 1)
+                all_outputs.append(predicted.cpu().numpy())
+                all_targets.append(batch_targets.cpu().numpy())
+
+            all_outputs = np.concatenate(all_outputs)
+            all_targets = np.concatenate(all_targets)
+            accuracy = accuracy_score(all_targets, all_outputs)
+            accHistory.append(accuracy)
+    #         accAverHis.append(np.mean(np.array(accHistory)))
+            print(f"acc_val: {str(accuracy)[0:7]}，",end="\t")
+            print('OK '+'='*20 if(np.max(np.array(accHistory)) == accuracy) else str(100*(accuracy - np.max(np.array(accHistory))))[0:5],end="\t")
+
+
+        if(epoch>50):
+            print(f"[{ str(np.abs(np.mean(lossHistory)-np.mean(lossHistory[0:epoch-50])))[0:4] }")
+    #         if(np.abs(np.mean(lossHistory)-np.mean(lossHistory[0:epoch-50]))<0.05):
+    #             if(np.abs(100*(accuracy - np.max(np.array(accHistory))))<5):
+    #                 print("已提前结束。")
+    #                 break
+
+
+        else:
+            print("")
+
+        early_stopping(accuracy, model)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break   
